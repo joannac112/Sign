dcf92726b72221636c9934609390b5d25a743316
joannac112 <joannac112@nycstudents.net>
2025-01-03 14:22:19 +0000

    jfdf

--- Sign.java
+++ Sign.java
@@ -2,12 +2,25 @@ public class Sign
 {
     private String message;
     private int width;
-    public Sign(string m, int w){
+    public Sign(String m, int w){
         message = m;
         width = w;
     }
 
     public int numberOfLines(){
-        return message.length()/width+1;
+        int lines = message.length() / width;
+        if(message.length() % width == 0) return lines;
+        return lines + 1;
     }
+    public String getLines(){
+        String s = "";
+        int i = 0;
+        if(message.length() == 0) return null;
+        while (i < message.length()){
+            s += message.substring(i, i + 1);
+            i++;
+            if(i % width == 0 && i != message.length()) s += ";";
+            }
+        return s;
+        }
 }
